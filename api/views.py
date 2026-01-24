49481472dcf895b5da8b48f3b0c80bf549b012a0
kapil shivhare <kapilshivhare9111728981@gmail.com>
2026-01-24 09:00:00 +0530

    Add Serializers, Views and URL routing for Projects and Contacts

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Project, Contact
+from .serializers import ProjectSerializer, ContactSerializer
 
 # Create your views here.
+class ProjectViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    A simple ViewSet for viewing projects.
+    """
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class ContactAPIView(APIView):
+    """
+    A View for creating contact messages.
+    """
+    def post(self, request):
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
