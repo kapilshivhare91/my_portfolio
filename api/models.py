641627b3a6be0dbde8cfdf4a6b661a0058cbe2de
kapil shivhare <kapilshivhare9111728981@gmail.com>
2026-01-28 14:40:04 +0530

    Add Project and Contact models

---
+++ api/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/') # Requires Pillow
+    tech_stack = models.JSONField(default=list) # Stores ["React", "Python"]
+    github_link = models.URLField()
+    demo_link = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name}"
